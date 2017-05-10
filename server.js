153f83d07cc314e6fb8a5d438de0abc277246812
Sampath Kumar Gajawada <gajawada.sampath@gmail.com>
2017-05-10 14:48:32 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+﻿
+
+//modules **************************************
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var request = require('request');
+
+
+// get all data/stuff of the body (POST) parameters
+// parse application/json 
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// expose app           
+exports = module.exports = app;
+
+//set our port
+//var port = process.env.PORT || 8080; 
+app.set('port', process.env.PORT || 8080);
+
+app.listen(app.get('port'));
+
+// shoutout to the user                     
+console.log('Application started on port ' + app.get('port'));
+
+var url = "https://dev19713.service-now.com/api/now/table/incident";
+request.get(url, {
+    'auth': {
+        'user': 'admin',
+        'pass': 'SAMPATH18',
+        'sendImmediately': false
+    }
+}).on('response', function (response) {
+    console.log(response.statusCode);
+    console.log(response.headers['content-type'])
+    response.on('data', function (data) {
+        console.log('data: ' + data);
+    })
+});
