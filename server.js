58eaada277abf61eb05d600b22dd815793df1407
Sampath Kumar Gajawada <gajawada.sampath@gmail.com>
2017-05-11 12:15:51 +0530

    ..

--- server.js
+++ server.js
@@ -54,3 +54,22 @@ app.get('/getAllIncidents', function (req, res) {
         })
     });
 });
+app.get('/incident', function (req, res) {
+    //res.send("hello");
+    var sysId = req.query.Id;
+    var url = "https://dev19713.service-now.com/api/now/table/incident"+"/"+sysId;
+    request.get(url, {
+        'auth': {
+            'user': 'admin',
+            'pass': 'SAMPATH18',
+            'sendImmediately': false
+        }
+    }).on('response', function (response) {
+        console.log(response.statusCode);
+        console.log(response.headers['content-type'])
+        response.on('data', function (data) {
+            console.log('data: ' + data);
+            res.write(data);
+        })
+    });
+});
