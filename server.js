157e88672240113f4d0b7036bd53e18e9c99bc49
Sampath Kumar Gajawada <gajawada.sampath@gmail.com>
2017-05-12 16:01:17 +0530

    ..

--- server.js
+++ server.js
@@ -169,13 +169,12 @@ function getIncidentInfo(sysId, assistant)
                 
                 var incident = JSON.parse(data);
                 console.log('data: ' + incident);
-                var parser = new JSONParser();
-                var parsedResponse = parser.parse(data);
+                
 
 
                 
                 //resolve(assistant.tell("Incident 1 Severity" + incident.result.agent));
-                resolve(assistant.tell("Incident 2 Severity" + parsedResponse[0].agent));
+                resolve(assistant.tell("Incident 2 Severity" + incident.result[0].agent));
               
                
 
