3e6a41366dcb23c26847610b495897e5e71b7744
Sampath Kumar Gajawada <gajawada.sampath@gmail.com>
2017-05-15 11:48:03 +0530

    ..

--- server.js
+++ server.js
@@ -55,100 +55,49 @@ app.get('/getAllIncidents', function (req, res) {
         })
     });
 });
-app.get('/incident', function (req, res) {
-    //res.send("hello");
-    //var sysId = req.query.Id;
-    var url = "https://dev19713.service-now.com/api/now/table/incident/d71f7935c0a8016700802b64c67c11c6";
-    request.get(url, {
-        'auth': {
-            'user': 'admin',
-            'pass': 'SAMPATH18',
-            'sendImmediately': false
-        }
-    }).on('response', function (response) {
-        console.log(response.statusCode);
-        console.log(response.headers['content-type'])
-        response.on('data', function (data) {
-            console.log('data: ' + data);
-            res.write(data.result);
-        })
-    });
-});
+
 
 
 
 // Create functions to handle requests here
 const WELCOME_INTENT = 'input.welcome';  // the action name from the API.AI intent
-const INCIDENT_INTENT = 'input.incident';  // the action name from the API.AI intent
+const PROBLEM_INTENT = 'input.problem';  // the action name from the API.AI intent
+const PROBLEM_ALL_INTENT = 'input.allproblems';  // the action name from the API.AI intent
 
 
 
 
 
 
-function sillyNameMaker(req, res) {
+function InitializeAssistant(req, res) {
     const assistant = new apiaiAssistant({ request: req, response: res });
-
-    
 
     var actionMap = new Map();
     actionMap.set(WELCOME_INTENT, welcomeIntent);
-    actionMap.set(INCIDENT_INTENT, incidentIntent);
+    actionMap.set(PROBLEM_INTENT, problemIntent);
+    actionMap.set(PROBLEM_ALL_INTENT, incidentIntent);
     //actionMap.set(NUMBER_INTENT, numberIntent);
     assistant.handleRequest(actionMap);
 
-
-    // you can add the function name instead of an action map
-    //assistant.handleRequest(responseHandler);
-}
-
-function responseHandler (assistant) {
-    console.log("okok")
-    // intent contains the name of the intent you defined in the Actions area of API.AI
-    assistant.ask('Welcome! Say a number.');
-    //let intent = assistant.getIntent();
-    //switch (intent) {
-    //    case WELCOME_INTENT:
-    //        assistant.ask('Welcome! Say a number.');
-    //        break;
-
-    //    case NUMBER_INTENT:
-    //        let number = assistant.getArgument(NUMBER_ARGUMENT);
-    //        assistant.tell('You said ' + number);
-    //        break;
-    //}
 }
 
 
-function incidentIntent(assistant)
-{
-    //return new Promise(function (resolve, reject) {
 
-        var sysId = assistant.getArgument('Id');
-        //resolve(assistant.tell('Incident severity ' ));
-        // assistant.tell('You Said' + sysId);
-       // assistant.handleRequest(getIncidentInfo(sysId));
-        return getIncidentInfo(sysId, assistant);
-        
-        //.then(function (data) {
+function problemIntent(assistant) {
+    var sysId = assistant.getArgument('Id');
+    return getProblemInfo(sysId, assistant);
 
-        //   // assistant.tell('Fetching incident information......................');
-        //    resolve(assistant.tell('Incident severity ' + data.severity));
-        //}, function (err) {
-        //    resolve(assistant.tell('Error occured ' ));
-        //});
-  //  });
+}
+function problemAllIntent(assistant) {
     
-    
-    
-        
+    return getAllProblems(assistant);
+
 }
 
-function welcomeIntent (assistant) {
+function welcomeIntent(assistant) {
     assistant.ask('Welcome to Servicenow chat service .');
 }
-function getIncidentInfo(sysId, assistant)
-{
+function getProblemInfo(sysId, assistant) {
     return new Promise(function (resolve, reject) {
         var str = '';
         var url = "https://dev19713.service-now.com/api/now/table/problem" + "/" + sysId;
@@ -164,15 +113,13 @@ function getIncidentInfo(sysId, assistant)
         }).on('response', function (response) {
             console.log(response.statusCode);
             console.log(response.headers['content-type']);
-          
+
             response.on('data', function (chunk) {
                 str += chunk;
             });
 
             response.on('end', function () {
-                //console.log(req.data);
-                console.log(str);
-                // your code here if you want to use the results !
+               
                 var problem = JSON.parse(str);
                 var speech = "This problem describes on " + problem.result.short_description + " with Urgency level " + problem.result.urgency + ", last updated on  " + problem.result.sys_updated_on + " and updated by " + problem.result.sys_updated_by;
                 resolve(assistant.tell(speech));
@@ -180,12 +127,50 @@ function getIncidentInfo(sysId, assistant)
         });
 
     });
-    
+
+}
+
+function getAllProblems(assistant)
+{
+    return new Promise(function (resolve, reject) {
+        var str = '';
+        var url = "https://dev19713.service-now.com/api/now/table/problem";
+        request.get(url, {
+            'auth': {
+                'user': 'admin',
+                'pass': 'SAMPATH18',
+                'sendImmediately': false
+            },
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }).on('response', function (response) {
+            console.log(response.statusCode);
+            console.log(response.headers['content-type']);
+
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            response.on('end', function () {
+
+                var problems = JSON.parse(str);
+                var speech = "Please find below problems ";
+
+                for (var i = 0 ; i < problems.result.length ; i++)
+                {
+                    speech = speech + problems.result[i].number +"describes on "+ problems.result[i].short_description;
+                }
+                resolve(assistant.tell(speech));
+            });
+        });
+
+    });
 }
 
 app.post('/google', function (req, res) {
-    console.log(req.body);
-    sillyNameMaker(req, res);
+    
+    InitializeAssistant(req, res);
 })
 
 
