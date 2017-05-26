d8ec2da7ab6a3ee53c714b63d45b7b4c9cfe87dd
Sampath Kumar Gajawada <gajawada.sampath@gmail.com>
2017-05-26 17:22:35 +0530

    ..

--- assistant/incidentAssistant.js
+++ assistant/incidentAssistant.js
@@ -1,17 +1,72 @@
-﻿const INCIDENT_INTENT = 'input.problem';  // the action name from the API.AI intent
+﻿const INCIDENT_INTENT = 'input.incident';  // the action name from the API.AI intent
 const INCIDENT_ALL_INTENT = 'input.incidents';  // the action name from the API.AI intent
-const INCIDENT_CREATE_INTENT = 'input.createproblem';  // the action name from the API.AI intent
+const INCIDENT_CREATE_INTENT = 'input.createincident';  // the action name from the API.AI intent
 
 
 var initializeIncident = function (actionMap) {
-    //actionMap.set(PROBLEM_INTENT, problemIntent);
+   
     actionMap.set(INCIDENT_ALL_INTENT, incidentAllIntent);
-   // actionMap.set(PROBLEM_CREATE_INTENT, problemCreateIntent);
+    actionMap.set(PROBLEM_CREATE_INTENT, incidentCreateIntent);
 };
+
+//function incidentAllIntent(assistant) {
+//    assistant.ask('Testing Incident from server');
+
+//};
 
 function incidentAllIntent(assistant) {
-    assistant.ask('Testing Incident from server');
+    var state = assistant.getArgument('state'),
+        urgency = assistant.getArgument('urgency'),
+        incidentNumber = assistant.getArgument('incidentNumber'),
+        filterData = { 'state': state, 'urgency': urgency, 'incidentNumber': incidentNumber };
 
-};
+
+    return new Promise(function (resolve, reject) {
+        var speech = "";
+        problemData.GetAllProblems(filterData)
+            .then(function (data) {
+
+                if (data.length <= 0) {
+                    speech = "Sorry!! Could not find the results";
+                } else if (data.length == 1) {
+                    speech = "The incident " + data[0].number + " describes on" + data[0].short_description + " with urgency level" + data[0].urgency;
+                }
+                else {
+                    speech = "Please find below data ";
+                    for (var i = 0 ; i < data.length ; i++) {
+                        speech = speech + " " + data[i].number + " describes on " + data[i].short_description;
+                    }
+                }
+
+                resolve(assistant.tell(speech));
+            }, function (err) {
+                cons
+                resolve(assistant.tell("Sorry!! some error occured in fetching tickets/incidents. Please try again!!"));
+            });
+
+    });
+
+}
+function incidentCreateIntent(assistant) {
+    var description = assistant.getArgument('description'),
+        urgency = assistant.getArgument('urgency');
+
+    var postData = { 'short_description': description, 'urgency': urgency };
+
+    return new Promise(function (resolve, reject) {
+
+        problemData.CreateProblem(postData)
+            .then(function (data) {
+                var speech = "Great!! Your ticket\incident was created which describes on " + data.short_description + " with Urgency level " + data.urgency + ", last updated on  " + data.sys_updated_on + " and updated by " + data.sys_updated_by;
+                resolve(assistant.tell(speech));
+            }, function (err) {
+
+                resolve(assistant.tell("Sorry!! some error occured in creating a incident. Please try again!!"));
+            });
+
+    });
+
+
+}
 
 module.exports.InitializeIncident = initializeIncident;
---
+++ constants.js
@@ -0,0 +1,45 @@
+﻿const urgency = {
+    High: 1,
+    Medium: 2,
+    Low: 3
+};
+
+const state = {
+    New: 1,
+    Active: 2,
+    AwaitingProblem: 3,
+    AwaitingUserInfo: 4,
+    AwaitingEvidence: 5,
+    Resolved: 6,
+    Closed:7
+
+};
+const impact = {
+    High: 1,
+    Medium: 2,
+    Low: 3
+};
+const priority = {
+    Critical: 1,
+    High: 2,
+    Moderate: 3,
+    Low: 4,
+    Planning:5,
+    Moderate: 3,
+};
+const category = {
+    Request: 1,
+    InquiryHelp: 2,
+    Software: 3,
+    Hardware: 4,
+    Network: 5,
+    Database:6
+};
+const contacttype = {
+    Email: 1,
+    Phone: 2,
+    SelfService: 3,
+    WalkIn:4
+
+
+};
